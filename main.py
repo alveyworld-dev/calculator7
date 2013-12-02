7a7c59c849852a0ac2dbb5bbc076a294acd40092
Joshua Beitler <joshbeitler@gmail.com>
2013-12-02 14:15:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ oooooh so cheaty
 runs all math through bc, giving precise
 floating point numbers for any and all
 equations
+
+http://www.gnu.org/software/bc/manual/html_mono/bc.html
 """
 
 math = raw_input("Input math: ")
