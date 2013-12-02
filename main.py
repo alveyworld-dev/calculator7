5d193471b9e9ab0695ab0bb23870cae4d5b4c038
Joshua Beitler <joshbeitler@gmail.com>
2013-12-02 14:11:25 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+oooooh so cheaty
+
+runs all math through bc, giving precise
+floating point numbers for any and all
+equations
+"""
+
+math = raw_input("Input math: ")
+program = "bc " + math + " | more"
+
+import subprocess
+subprocess.call([more])
