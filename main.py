07aa3a70bbf649232e5f4d4f5b0d48a2ffd9652d
BrandonBench <thechummiest@gmail.com>
2013-12-04 13:15:05 -0700

    setting up main()

---
+++ main.py
@@ -0,0 +1,18 @@
+
+def main():
+	
+
+
+if __name__ == '__main__':
+	main()
+
+#from team1 import *
+from team2 import *
+from team3 import *
+#from team4 import *
+#from team5 import *
+from team6 import *
+
+print add(1,2,3)
+print sub(3,2,1)
+print opp(4)
