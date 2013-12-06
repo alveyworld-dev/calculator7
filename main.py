f61d6c5a050b6fff0039e3e65492badf5dc0d533
souleater727 <luiscanales117@gmail.com>
2013-12-06 13:02:23 -0700

    getting rid of the pony and working with main

--- main.py
+++ main.py
@@ -1,104 +1,13 @@
-# My Little Alvey - Friendship is Magic
-	
-"""
-__________________________________▓▓▓▓▓▓
-_________________________▓▓▓▓▓▓░░░░░░░▓▓▓▓▓
-_____________________▓▓▓▒▒▒▒▒▒▒░░░░░▓▓▓▓▓▓▓▓▓
-______▒▒_________▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒░░░░░▓___▓▓▓
-____▒▒▒▒▒___▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒░░░▓
-___▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▓▒▒░▓▓
-__▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▓▒▒▒▓▓
-__▒▒▒▓▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▓▓▒▒▒▓
-_▒▒▒▒▓▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▓▒▒▒▓
-_▒▒▒▒▓▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▓▓▓▓▓
-__▒▒▒▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▓_____▓
-__▒▒▒▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▓
-___▒▒▒▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▒▒▒▓▓▓▓▓▒▒▒▒▒▒▓
-____▒▒▒▒▒▒▅▅▄▒▒▒▒▒▒▒▒▒▒▒▓▓▓▒▒▒▒▒▒▓▓▓▒▒▒▒▒▓▓
-___▓░▒▒▒▒▅▅▅▅█▀▀▀▅▅▅▅▄▄▒▒▒▒▓▒█▒▒▄▒▒▒▄▅▓▒▒▒▒▒▓
-___▓░░▒▒▒▒▄▅▅▌________▓_▀▀██▅▄█▒▒▀▀▀__█
-__▓░░░░▓▒▒▒▒▒_________▓____▐██▒▒▒▒▒▌▓__█
-__▓░░░░▓▒▒▒▒▒_________▓__▄███▌▒▒▒▒▐_▓██
-__▓░░░░░▓▒▒▒▒▒________▓▓█__██▒▒▒▒▒▒▓▌▐
-_▓▒▒░░░░░▓▒▒▒▒▒_______▓█████▒▒▒▒▒▒▒██
-_▓▒▒░░░░░░▓▓▓▒▒▒▒_____▓███▀▒▒▒▒▒▒▒▒▒▒▒
-_▓▒▒▒░░░░░░░░░▓▓▓▓▒▒__▓▒▒▒▒▒▒▒_▒▒▀▀▒▒
-_▓▒▒▒▒░░░░░░░░░▓▓▒▒▒▒▒▒▒▒▒▒▒▒___▒▒▒▒
-__▓▒▒▒▒▒░░░░▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒__▄▀
-__▓▓▒▒▒▒▒░░░░░▓____▒▒▒▒▒▒▒▒▒▒▒▀
-___▓▓▒▒▒▒▒▒░░░░░▓
-___▓▓▓▒▒▒▒▒▒▒░░░░▓
-____▓▓▓▒▒▒▒▒▒▒░░░░▓
-____▓▓▓▓▓▒▒▒▒▒▒▒░░▓
-______▓▓▓▓▓▒▒▒▒▒▒▒░▓
-______▓▓▓▓▓▓▒▒▒▒__▓▓
-________▓▓▓▓▓▓▒▒▓___▓
-_________▓▓▓▓▓▓▒▒▓
-__________▓▓▓▓▓▓▒▓
-___________▓▓▓▓▓▓▓
-____________▓▓▓▓▓
-____________▓▓▓▓
-___________▓▓▓
-__________▓▓
-"""
-
-if __name__ == '__main__':
-	pass
-
 #from team1 import *
 from team2 import *
 from team3 import *
 #from team4 import *
 #from team5 import *
 from team6 import *
+  
+def main():
+	pass
+	
+if __name__ == "__main__":
+	pass 
 
-print add(1,2,3) #should equal 6
-print sub(3,2,1) #should equal 0
-print opp(4)     #should equal -4
-print pow(2,3)   #should equal 8
-print fact(3)    #should equal 6
-
-
-"""
-TWILIGHT SPARKLE:
-My Little Pony, My Little Pony, Ah Aah Aah Aah
-
-BACKUP SINGERS:
-My Little Pony
-
-TWILIGHT SPARKLE:
-I used to wonder what friendship could be 
-
-BACKUP SINGERS:
-My Little Pony
-
-TWILIGHT SPARKLE:
-Until you all shared its magic with me!
-
-RAINBOW DASH:
-Big adventure
-
-PINKIE PIE:
-Tons of fun
-
-RARITY:
-A beautiful heart
-
-APPLEJACK:
-Faithful and strong
-
-FLUTTERSHY:
-Sharing kindness 
-
-Twilight Sparkle:
-It's an easy fee!
-
-All:
-And magic makes it all complete, yeah my little pony
-
-TWILIGHT SPARKLE:
-Do You Know You're All My very Best...
-
-All: 
-Friends!!!!
-"""
